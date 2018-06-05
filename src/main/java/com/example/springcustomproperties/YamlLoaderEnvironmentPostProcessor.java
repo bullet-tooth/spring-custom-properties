cfed990211e15528409007c35692b32de0868995
Oleg Bondar <bullet-tooth@users.noreply.github.com>
2018-06-05 12:25:12 +0300

    Fixed yaml properties loading issue

---
+++ src/main/java/com/example/springcustomproperties/YamlLoaderEnvironmentPostProcessor.java
@@ -0,0 +1,52 @@
+package com.example.springcustomproperties;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.env.EnvironmentPostProcessor;
+import org.springframework.boot.env.YamlPropertySourceLoader;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.DefaultResourceLoader;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.core.io.support.ResourcePatternUtils;
+
+public class YamlLoaderEnvironmentPostProcessor implements EnvironmentPostProcessor {
+
+  private final YamlPropertySourceLoader loader = new YamlPropertySourceLoader();
+  private final ResourceLoader resourceLoader = new DefaultResourceLoader();
+
+  @Override
+  public void postProcessEnvironment(ConfigurableEnvironment environment,
+      SpringApplication application) {
+
+    loadResources().stream()
+        .map(this::loadYaml)
+        .forEach(environment.getPropertySources()::addLast);
+  }
+
+  private PropertySource<?> loadYaml(Resource path) {
+    if (!path.exists()) {
+      throw new IllegalArgumentException("Resource " + path + " does not exist");
+    }
+    try {
+      return this.loader.load("custom-resource", path).get(0);
+    } catch (IOException ex) {
+      throw new IllegalStateException(
+          "Failed to load yaml configuration from " + path, ex);
+    }
+  }
+
+  private List<Resource> loadResources() {
+    try {
+      return Arrays.asList(ResourcePatternUtils.getResourcePatternResolver(resourceLoader)
+          .getResources("classpath*:config/*.yml"));
+    } catch (IOException e) {
+      throw new IllegalStateException(
+          "Failed to read yaml configuration file", e);
+    }
+  }
+
+}
--- src/main/java/com/example/springcustomproperties/property/MyProperty.java
+++ src/main/java/com/example/springcustomproperties/property/MyProperty.java
@@ -3,12 +3,10 @@ package com.example.springcustomproperties.property;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
 @Data
 @Configuration
 @ConfigurationProperties(prefix = "my-property")
-@PropertySource("classpath:config/custom.yml")
 public class MyProperty {
 
   private String value;
